db44d56a4276f71e8c64848a1744200649989871
unknower <bangwhe@qq.com>
2020-09-22 19:22:04 +0800

    添加增加标签和修改标签计数

--- main_window.py
+++ main_window.py
@@ -16,6 +16,8 @@ class MainWindow(QtWidgets.QMainWindow, ui_main_window.Ui_MainWindow):
         self.img_list = None    # 图片路径下的所有图片名称数组
         self.img_name = None    # 当前显示图片的名称
         self.labels = None      # 所有图片的标签
+        self.labeled_img = 0
+        self.edited_img = 0
         self.username = username
         super(MainWindow, self).__init__(parent)
         self.setWindowTitle(__appname__)
@@ -36,6 +38,10 @@ class MainWindow(QtWidgets.QMainWindow, ui_main_window.Ui_MainWindow):
         self.photo.setTabletTracking(False)
         self.photo.setObjectName("photo")
         self.horizontalLayout.addWidget(self.photo)
+
+        # 必须从int强转成string
+        self.labeled_number_label.setText(str(self.labeled_img))
+        self.edit_number_label.setText(str(self.edited_img))
 
     def setListener(self):
         # 设置强焦点事件, 只能通过TAB和鼠标获取焦点, 保证能使用空格切换图片
@@ -116,13 +122,23 @@ class MainWindow(QtWidgets.QMainWindow, ui_main_window.Ui_MainWindow):
             # 如果前后的标签不一致
             if label != old_label:
                 try:
+                    # 图片存在, 说明标签被修改
                     shutil.move(os.path.join(self.save_dir, labels[old_label], self.img_name),
                                 os.path.join(self.save_dir, labels[label], self.img_name))
+                    self.edited_img += 1
+                    self.edit_number_label.setText(f'{self.edited_img}')
                 except FileNotFoundError as e:
+                    # 图片不存在, 说明新打了这张图片
                     shutil.copy(os.path.join(self.img_dir, self.img_name), os.path.join(self.save_dir, labels[label]))
+                    self.labeled_img += 1
+                    self.labeled_number_label.setText(f'{self.labeled_img}')
+
             # 如果一致, 但是没有图片, 也需要修改
             elif self.img_name not in os.listdir(os.path.join(self.save_dir, labels[label])):
                 shutil.copy(os.path.join(self.img_dir, self.img_name), os.path.join(self.save_dir, labels[label]))
+
+                self.labeled_img += 1
+                self.labeled_number_label.setText(f'{self.labeled_img}')
             return True
         return False
 
